f6819b6393505db9e4e916851d7ceb319b5a0fa2
amy1012 <134953397+amy1012@users.noreply.github.com>
2024-12-21 15:58:58 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+  <div class="container">
+    <!-- 建立第一個 row 空間，裡面準備放格線系統 -->
+    <div class="row">
+      <!-- 在 xs 尺寸，佔12格，可參考 http://getbootstrap.com/css/#grid 說明-->
+      <div class="col-xs-12"  style="height: 80px;"  >
+        <hr>
+        <p class="text-center" style="font-size: 14px; align-items: center; margin: 0px;">
+          &copy; <?php echo date("Y")
+          ?>
+          NSBlog.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
